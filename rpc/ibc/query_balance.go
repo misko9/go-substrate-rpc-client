5bd2aea337ae1f169d86d98828e83128acfe3586
Steve Miskovetz <steve.miskovetz@gmail.com>
2023-04-13 15:53:36 -0600

    Add denom to query balance api

--- rpc/ibc/query_balance.go
+++ rpc/ibc/query_balance.go
@@ -9,12 +9,13 @@ import (
 func (i IBC) QueryBalanceWithAddress(
 	ctx context.Context,
 	addr string,
+	denom uint64,
 ) (
 	sdk.Coin,
 	error,
 ) {
 	var res sdk.Coin
-	err := i.client.CallContext(ctx, &res, queryBalanceWithAddressMethod, addr)
+	err := i.client.CallContext(ctx, &res, queryBalanceWithAddressMethod, addr, denom)
 	if err != nil {
 		return sdk.Coin{}, err
 	}
